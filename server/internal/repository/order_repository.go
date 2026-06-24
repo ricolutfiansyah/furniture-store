d6f5671e6433e29c4e556fcb3a6df6b9f841489d
ricolutfiansyah <rahmandarico3@gmail.com>
2026-06-24 12:36:07 +0700

    feat: Implement CreateOrderWithTx method for order creation with transaction support

--- server/internal/repository/order_repository.go
+++ server/internal/repository/order_repository.go
@@ -1,6 +1,11 @@
 package repository
 
-import "github.com/jmoiron/sqlx"
+import (
+	"context"
+	"furniture-api/internal/domain"
+
+	"github.com/jmoiron/sqlx"
+)
 
 type orderRepository struct {
 	db *sqlx.DB
@@ -8,4 +13,32 @@ type orderRepository struct {
 
 func NewOrderRepository(db *sqlx.DB) *orderRepository {
 	return &orderRepository{db: db}
+}
+
+func (r *orderRepository) CreateOrderWithTx(ctx context.Context, tx *sqlx.Tx, order *domain.Order) error {
+	query := `
+		INSERT INTO orders
+			(user_id, order_number, total_amount, shipping_cost, tax, grand_total, 
+			status, shipping_address, payment_method, notes
+		) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`
+	result, err := tx.ExecContext(ctx, query,
+		order.UserID,
+		order.OrderNumber,
+		order.TotalAmount,
+		order.ShippingCost,
+		order.Tax,
+		order.GrandTotal,
+		order.Status,
+		order.ShippingAddress,
+		order.PaymentMethod,
+		order.Notes,
+	)
+	if err != nil {
+		return err
+	}
+
+	id, _ := result.LastInsertId()
+	order.ID = int(id)
+	return nil
 }
